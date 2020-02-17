4600ea9bdf26948c3a98dc5493a9aea362f9f6c0
Takeallah <takeallahsrivera@icloud.com>
2020-02-17 14:09:26 -0600

    ClassworkPushThree

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ if(myFirstName = myFirstName2){
 
 }
 
+//Problem 2
+
+let theNumbers = prompt("Enter a number");
+if(theNumbers = 1){
+    console.log("The number is positive!!");
+
+} else if(theNumbers = -1){
+    console.log("The number is negative!!");
+} 
+
+//Problem 3 
+let enterNumber = prompt("Enter a number");
+let enterAnotherNumber = prompt("Enter another number");
+
+if(enterNumber>= 18){
+    console.log("You can vote!!");
+
+} else if(age <18){
+    console.log("You can not vote!");
+}
+
