506015ccaa84fb369ff7890d06daed3fd5560f23
autumn-ragland <raglanda998@gmail.com>
2020-02-17 16:56:36 -0600

    graded

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 //Problem 1 
 
-let myFirstName = "Takeallah";
-let myFirstName2 = "Takeallah";
+// let myFirstName = "Takeallah";
+// let myFirstName2 = "Takeallah";
 
 
-if(myFirstName = myFirstName2){
-    console.log("That is correct!!!");
+// if(myFirstName = myFirstName2){
+//     console.log("That is correct!!!");
 
-} else if(myFirstName != myFirstName2){
-    console.log("That is not correct!!!");
+// } else if(myFirstName != myFirstName2){
+//     console.log("That is not correct!!!");
 
-}
+// }
 
 //Problem 2
 
-let theNumbers = prompt("Enter a number");
-if(theNumbers = 1){
-    console.log("The number is positive!!");
+// let theNumbers = prompt("Enter a number");
+// // a single equal sign sets a value and a double equal sign compares value
+// if(theNumbers = 1){
+//     console.log("The number is positive!!");
 
-} else if(theNumbers = -1){
-    console.log("The number is negative!!");
-} 
+// } else if(theNumbers = -1){
+//     console.log("The number is negative!!");
+// } 
 
 //Problem 3 
-let enterNumber = prompt("Enter a number");
-let enterAnotherNumber = prompt("Enter another number");
+// let enterNumber = prompt("Enter a number");
+// let enterAnotherNumber = prompt("Enter another number");
 
-if(enterNumber + enterAnotherNumber= >= 18){
-    console.log("You can vote!!");
+// if(enterNumber + enterAnotherNumber= >= 18){
+//     console.log("You can vote!!");
 
-} else if(enternumber + enterAnotherNumber= <18){
-    console.log("You can not vote!");
-}
+// } else if(enternumber + enterAnotherNumber= <18){
+//     console.log("You can not vote!");
+// }
 
