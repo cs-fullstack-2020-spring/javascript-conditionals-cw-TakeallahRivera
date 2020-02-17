3163e94fe223c4a96a25e84c1404d091b0abf024
Takeallah <takeallahsrivera@icloud.com>
2020-02-17 13:31:44 -0600

    ClassworkPushTwo

---
+++ script.js
@@ -0,0 +1,14 @@
+//Problem 1 
+
+let myFirstName = "Takeallah";
+let myFirstName2 = "Takeallah";
+
+
+if(myFirstName = myFirstName2){
+    console.log("That is correct!!!");
+
+} else if(myFirstName != myFirstName2){
+    console.log("That is not correct!!!");
+
+}
+
